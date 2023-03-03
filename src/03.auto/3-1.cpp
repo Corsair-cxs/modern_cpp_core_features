e4c59767b9bedad373edde0770b2a368a66e90d6
Corsair-cxs <1494089474@qq.com>
2023-03-04 06:03:55 +0800

    Modify code, compile passed

---
+++ src/03.auto/3-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+template<auto N>
+void f()
+{
+	std::cout << N << std::endl;
+}
+
+int main()
+{
+	f<5>();     // N为int类型
+	f<'c'>();   // N为char类型
+	f<50>();   // 编译失败，模板参数不能为double
+}
